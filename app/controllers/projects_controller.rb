e0bf8dfb53e577b1cbe88fb67e9fa70d136b53ef
Jorge Juan <hi.jorgejuanp@gmail.com>
2016-02-27 12:38:00 +0100

    Add project creation

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,49 @@
+class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project, notice: "El proyecto se ha creado correctamente"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to root_path, notice: "Project deleted"
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
+end
