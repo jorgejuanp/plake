d8a49d29336f93f68e517e725b6c881c3e1e500b
Jorge Juan <hi.jorgejuanp@gmail.com>
2016-03-01 05:33:03 +0100

    Add profile images (avatars)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ActiveRecord::Base
   has_many :projects
 
   #paperclip
-  has_attached_file :avatar, styles: { thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  has_attached_file :avatar,
+    :styles => { :medium => "300x300>", :thumb => "30x30#" },
+    :default_url => "/images/:style/missing.png",
+    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+    :url => "/system/:attachment/:id/:style/:filename"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
