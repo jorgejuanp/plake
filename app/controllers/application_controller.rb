375666a5ea417b993516040414e95cd11f9c3855
Jorge Juan <hi.jorgejuanp@gmail.com>
2016-03-02 05:52:09 +0100

    Add User fields: username, description, link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:avatar, :username, :description, :link, :email, :password, :current_password) }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227152351) do
+ActiveRecord::Schema.define(version: 20160302041504) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -38,6 +38,9 @@ ActiveRecord::Schema.define(version: 20160227152351) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "username"
+    t.text     "description"
+    t.string   "link"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
